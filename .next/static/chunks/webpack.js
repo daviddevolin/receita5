24528452d3a6a580a8249bc25e5b22c3962d63c2
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-11-18 11:45:23 -0300

    implementando cards no onemovie

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7bbf28d06a137879"; }
+/******/ 		__webpack_require__.h = function() { return "789f29467486fca3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/onemovie/[id].js
+++ pages/onemovie/[id].js
@@ -1,16 +1,27 @@
-import useSWR from 'swr'
-import { useRouter } from 'next/router'
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { Card } from 'antd';
+import "antd/dist/antd.css";
+
 export default function MovieID(){
     const router = useRouter()
     const {id} = router.query
     const {data, error} = useSWR(`https://www.omdbapi.com/?apikey=5d61b462&i=${id}`, fetcher)
+    const { Meta } = Card;
     if (error) return <div>falha na requisição...</div>
     if (!data) return <div>carregando...</div>
   
     return (
       <div id="content">
-        Titulo: {data.Title} -- Ano: {data.Year} -- 
-        <img src={data.Poster}></img>
+        <Card
+            hoverable
+            style={{
+                width: 240,
+            }}
+            cover={<img src={data.Poster}/>}
+        >
+            <Meta  title={data.Title} description={data.Year} />
+        </Card>
       </div>  
     )    
   }
