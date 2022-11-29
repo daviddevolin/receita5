451948c2ff46a36d1264b24f12678ce420f79b2f
david santos <56547716+daviddevolin@users.noreply.github.com>
2022-11-29 11:12:31 -0300

    implementando input

--- pages/searchmovies/index.js
+++ pages/searchmovies/index.js
@@ -1,10 +1,17 @@
-import { Space } from "antd";
+import { Space, Input, Button } from "antd";
+import { SearchOutlined } from '@ant-design/icons';
 import "antd/dist/antd.css";
 export default function Search(){
+    const onClickHandler = (e) => {
+        e.preventDefault() 
+        const value = document.getElementById('inputFilme').value;
+        window.location.href= `/searchmovies/${value}`
+    }
     return(
         <div> 
             <Space direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
-                <h1>digite o filme no fim da url "/nomedofilme"</h1>
+                <Input placeholder="digite o nome do filme"  id="inputFilme"/>    
+                <Button type='primary'  icon={<SearchOutlined />} onClick={onClickHandler} id='buttonInput'> pesquisar</Button>
             </Space>
             
         </div>
